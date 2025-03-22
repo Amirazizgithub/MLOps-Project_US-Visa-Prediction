7c2c5a96db0e671d7c195f4be698f614ae000acb
Amir Aziz <Amira@cmrsl.net>
2025-03-22 16:01:13 +0530

    RUN CICD PIPELINE

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import Response
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from starlette.responses import HTMLResponse, RedirectResponse
 from uvicorn import run as app_run
 
 from typing import Optional
